6922afa31f7232a9cff4fe40e919d0bcd4865a17
Tahsincan Köse <tahsinkose@yandex.com>
2018-03-29 20:01:13 +0300

    Create euler1.py

---
+++ euler1.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import sys
+
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+	n = int(raw_input().strip())
+	n-=1
+	result = 0
+	t_terms = (n-3)/3 + 1
+	f_terms = (n-5)/5 + 1
+	ff_terms = (n-15)/15 + 1
+	result += t_terms*(n+3-(n%3))/2
+	
+	result += f_terms*(n+5-(n%5))/2
+	
+	result -= ff_terms*(n+15-(n%15))/2
+	print result
